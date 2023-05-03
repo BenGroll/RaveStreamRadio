2153bb0ca7db8a3662c9e4305ca0ae190db22bf2
IfClause <ifclause002@gmail.com>
2023-05-03 02:03:29 +0200

    Refreshindicator on GroupView

--- lib/screens/overviewpages/groupoverviewpage.dart
+++ lib/screens/overviewpages/groupoverviewpage.dart
@@ -23,17 +23,33 @@ class GroupOverviewPage extends StatelessWidget {
           future: db.getGroup(groupid),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
-              return Scaffold(
-                backgroundColor: cl.darkerGrey,
-                appBar: AppBar(
-                  centerTitle: true,
-                  title: Text("Group: ${snapshot.data!.groupid}"),
-                  actions: [
-                    ReportButton(target: "${branchPrefix}groups/$groupid")
-                  ],
-                ),
-                body: snapshot.data != null ? GroupView(initialGroup: snapshot.data ?? dbc.demoGroup) : Text("Couldn't load group $groupid")
-              );
+              ValueNotifier<dbc.Group?> groupNotifier =
+                  ValueNotifier<dbc.Group?>(snapshot.data);
+              return ValueListenableBuilder(
+                  valueListenable: groupNotifier,
+                  builder: (context, group, foo) {
+                    return Scaffold(
+                        backgroundColor: cl.darkerGrey,
+                        appBar: AppBar(
+                          centerTitle: true,
+                          title: Text(
+                              "Group: ${group?.groupid ?? 'Not Loadable'}"),
+                          actions: [
+                            ReportButton(
+                                target: "${branchPrefix}groups/$groupid")
+                          ],
+                        ),
+                        body: snapshot.data != null
+                            ? RefreshIndicator(
+                                onRefresh: () async {
+                                  groupNotifier.value =
+                                      await db.getGroup(groupid);
+                                },
+                                child: GroupView(
+                                    initialGroup:
+                                        snapshot.data ?? dbc.demoGroup))
+                            : Text("Couldn't load group $groupid"));
+                  });
             } else {
               return Scaffold(
                   backgroundColor: cl.darkerGrey,
@@ -51,126 +67,119 @@ class GroupView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-      ValueNotifier<dbc.Group> group = ValueNotifier<dbc.Group>(initialGroup);
-      bool isGroupAdmin = 
-      currently_loggedin_as.value != null &&
-      group.value.members_roles != null &&
-          group.value.members_roles!
-              .containsKey(db.db.doc(currently_loggedin_as.value!.path)) &&
-          group.value.members_roles![db.db.doc(currently_loggedin_as.value!.path)] == "Founder";
-      return Scaffold(
-          backgroundColor: cl.darkerGrey,
-          body: SingleChildScrollView(
-              child: Container(
-                  padding: const EdgeInsets.all(16),
-                  child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Center(
-                          child: Container(
-                            width: 150,
-                            child: const CircleAvatar(
-                              radius: 70,
-                              /*backgroundImage: SvgPicture(pictureProvider),*/
-                            ),
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(
-                                color: cl.lighterGrey,
-                                width: 5.0,
-                              ),
+    ValueNotifier<dbc.Group> group = ValueNotifier<dbc.Group>(initialGroup);
+    bool isGroupAdmin = currently_loggedin_as.value != null &&
+        group.value.members_roles != null &&
+        group.value.members_roles!
+            .containsKey(db.db.doc(currently_loggedin_as.value!.path)) &&
+        group.value
+                .members_roles![db.db.doc(currently_loggedin_as.value!.path)] ==
+            "Founder";
+    return Scaffold(
+        backgroundColor: cl.darkerGrey,
+        body: SingleChildScrollView(
+            child: Container(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Container(
+                          width: 150,
+                          child: const CircleAvatar(
+                            radius: 70,
+                            /*backgroundImage: SvgPicture(pictureProvider),*/
+                          ),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: cl.lighterGrey,
+                              width: 5.0,
                             ),
                           ),
                         ),
-                        // Avatar Editor Icon
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            if (isGroupAdmin)
-                              IconButton(
-                                  onPressed: () {},
-                                  icon: Icon(
-                                    Icons.edit,
-                                    color: cl.darkerGrey,
-                                  )),
-                            ValueListenableBuilder(
-                                valueListenable: group,
-                                builder: (context, groupVal, foo) {
-                                  return Text(group.value.title ?? "Title",
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: MediaQuery.of(context)
-                                                  .size
-                                                  .width /
-                                              20));
+                      ),
+                      // Avatar Editor Icon
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          if (isGroupAdmin)
+                            IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.edit,
+                                  color: cl.darkerGrey,
+                                )),
+                          ValueListenableBuilder(
+                              valueListenable: group,
+                              builder: (context, groupVal, foo) {
+                                return Text(group.value.title ?? "Title",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize:
+                                            MediaQuery.of(context).size.width /
+                                                20));
+                              }),
+                          if (isGroupAdmin)
+                            IconButton(
+                                icon: Icon(
+                                  Icons.edit,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () async {
+                                  ValueNotifier<String?> titleNot =
+                                      ValueNotifier<String?>(group.value.title);
+                                  await showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return cw.SimpleStringEditDialog(
+                                            to_notify: titleNot);
+                                      });
+
+                                  if (titleNot.value != null &&
+                                      titleNot.value != group.value.title) {
+                                    group.value.title = titleNot.value;
+                                    await db.db
+                                        .doc(
+                                            "${branchPrefix}groups/${group.value.groupid}")
+                                        .update({"title": group.value.title});
+                                    group.notifyListeners();
+                                  }
                                 }),
-                            if (isGroupAdmin)
-                              IconButton(
-                                  icon: Icon(
-                                    Icons.edit,
-                                    color: Colors.white,
-                                  ),
-                                  onPressed: () async {
-                                    ValueNotifier<String?> titleNot =
-                                        ValueNotifier<String?>(
-                                            group.value.title);
-                                    await showDialog(
-                                        context: context,
-                                        builder: (BuildContext context) {
-                                          return cw.SimpleStringEditDialog(
-                                              to_notify: titleNot);
-                                        });
-
-                                    if (titleNot.value != null &&
-                                        titleNot.value !=
-                                            group.value.title) {
-                                      group.value.title = titleNot.value;
-                                      await db.db
-                                          .doc(
-                                              "${branchPrefix}groups/${group.value.groupid}")
-                                          .update(
-                                              {"title": group.value.title});
-                                      group.notifyListeners();
-                                    }
-                                  }),
-                          ],
-                        ),
-                        Row(
+                        ],
+                      ),
+                      Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
                             TextButton(
-                            onPressed: () {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  cw.hintSnackBar(
-                                      "Viewing of Event-List is WIP."));
-                            },
-                            child: Text(
-                                "Hosted events: ${group.value.events.length}",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize:
-                                        MediaQuery.of(context).size.width /
-                                            25))),
+                                onPressed: () {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                      cw.hintSnackBar(
+                                          "Viewing of Event-List is WIP."));
+                                },
+                                child: Text(
+                                    "Hosted events: ${group.value.events.length}",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize:
+                                            MediaQuery.of(context).size.width /
+                                                25))),
                             TextButton(
-                            onPressed: () {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  cw.hintSnackBar(
-                                      "Viewing of Member-List is WIP."));
-                            },
-                            child: Text(
-                                "Members: ${group.value.members_roles?.length ?? 0}",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize:
-                                        MediaQuery.of(context).size.width /
-                                            25)))
-                                            
-                                            ]
-                        )
-                        
-                      ]))));
-    }
+                                onPressed: () {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                      cw.hintSnackBar(
+                                          "Viewing of Member-List is WIP."));
+                                },
+                                child: Text(
+                                    "Members: ${group.value.members_roles?.length ?? 0}",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize:
+                                            MediaQuery.of(context).size.width /
+                                                25)))
+                          ])
+                    ]))));
+  }
 }
-
